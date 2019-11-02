b67e96ea1ed8e70d72ce6d2fb122c7ac32249e6e
Dimas <zhuravel16@gmail.com>
2019-11-02 17:48:55 +0200

    almost done slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,25 @@
-$(function(){
+$(function () {
 
-    $('.heaer__slider').slick({
-        infinite: true,
-        fade: true,
-        prevArrow: '<img class="slider__arrows slider__arrow-left"src="img/arrows-left.svg" alt="arrows-left">',
-        nextArrow: '<img class="slider__arrows slider__arrow-right"src="img/arrows-right.svg" alt="arrows-right">',
-        asNavFor: '.slider__dots'
-    });
-    $('.slider__dots').slick({
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      asNavFor: '.heaer__slider',
-      centerMode: false,
-      focusOnSelect: true
-    });
-        
+  $('.heaer__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider__arrows slider__arrow-left"src="img/arrows-left.svg" alt="arrows-left">',
+    nextArrow: '<img class="slider__arrows slider__arrow-right"src="img/arrows-right.svg" alt="arrows-right">',
+    asNavFor: '.slider__dots'
+  });
+  $('.slider__dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.heaer__slider',
+    centerMode: false,
+    focusOnSelect: true
+  });
 
-  
+
+  $('.surf__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider__arrows slider__arrow-left"src="img/arrows-left.svg" alt="arrows-left">',
+    nextArrow: '<img class="slider__arrows slider__arrow-right"src="img/arrows-right.svg" alt="arrows-right">',
+  })
 });
